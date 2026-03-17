9c6884f365a8736ef6bd20b45b862f0e573e74d2
marketingjosy2025-droid <marketingjosy2025@gmail.com>
2026-03-16 23:40:32 -0300

    Set up Flask with SocketIO for messaging

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Mensagem: ' + msg)
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
+  
